10030ee321b670c980513388a688f5f7355fd9ac
うさまるってぃ <goodtreeisagoodshelter@gmail.com>
2020-05-26 15:02:11 +0900

    完成初版
    プレイヤーとか点数とかは一切なくただただめくっていくだけの神経衰弱です。

---
+++ main.js
@@ -0,0 +1,75 @@
+'use strict';
+window.onload=function(){
+  function Card(suit,num){
+    this.suit=suit;
+    this.num=num;
+    //以下を追記
+    this.front;
+    this.setFront=function(){
+      this.front=`card_${this.suit}${this.num<10?'0':''}${this.num}.png`;
+    };
+  }
+
+  const cards=[];
+  const suits=['spade_','diamond_','heart_','club_'];
+  for(let i=0;i<suits.length;i++){
+    for(let j=1;j<=13;j++){
+      let card=new Card(suits[i],j);
+      //以下を追加
+      card.setFront();
+      cards.push(card);
+    }
+  }
+
+  function shuffle(){
+    let i=cards.length;
+    while(i){
+      let index=Math.floor(Math.random()*i--);
+      var temp=cards[index];
+      cards[index]=cards[i];
+      cards[i]=temp;
+      
+    }
+  }
+
+  shuffle();
+  const table=document.getElementById('table');
+  for(let i=0;i<suits.length;i++){
+    let tr=document.createElement('tr');
+    for(let j=0;j<13;j++){
+      let td=document.createElement('td');
+      let tempCard=cards[i*13+j];
+      //以下を追記
+      td.classList.add('card','back');
+      td.onclick=flip;
+      td.num=tempCard.num;
+      td.style.backgroundImage=`url(images/${tempCard.front})`;
+      tr.appendChild(td);
+    }
+    table.appendChild(tr);
+  }
+  let firstCard=null;
+  let flipTimerId=NaN;
+
+function flip(e){
+  let td=e.target;
+  if(!td.classList.contains('back') || flipTimerId){
+    return;
+  }
+  td.classList.remove('back');
+  if(firstCard===null){
+    firstCard=td;
+  }else{
+    if(firstCard.num===td.num){
+      firstCard=null;
+    }else{
+      flipTimerId=setTimeout(function(){
+        firstCard.classList.add('back');
+        td.classList.add('back');
+        flipTimerId=NaN;
+        firstCard=null;
+    },1000);
+  }
+}
+}
+}
